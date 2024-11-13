e19d55fac3dde2ea73a4ba904248785f0626eab3
bsule <bilal.suleiman@gmail.com>
2024-11-13 14:49:07 -0800

    cleanup

--- config.py
+++ config.py
@@ -1,15 +1,8 @@
 # global functions for easy access from all files
 # to access from the app folder do: from config import *
 def set_logfile_path(path):
-    """
-    Set the global logfile_path variable.
-
-    Args:
-    path (str): The absolute path of the logfile.
-    """
     global logfile_path
     logfile_path = path
     
 def get_logfile_path():
-    print(logfile_path)
     return logfile_path
