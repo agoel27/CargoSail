fc385184a0c35a590c973ad7fddea30efb8a6a2f
G-Fabian <fabiangtech@gmail.com>
2024-11-14 23:51:09 -0800

    quick fix: logfile.py now import functions from config.py

--- app/logfile.py
+++ app/logfile.py
@@ -1,18 +1,10 @@
 import os
+from config import *
 
 def add_logEntry(message):
-    # store localappdata directory
-    appdata_path = os.getenv("LOCALAPPDATA")
     
-    # if local app data directory cant be found, get desktop
-    if appdata_path is None:
-        appdata_path = os.path.normpath(os.path.expanduser("~/Desktop"))
-    
-    # store CargoSail folder on either localappdata or desktop
-    cargosail_folder = os.path.join(appdata_path, 'CargoSail')
- 
     # store CargoSail logpath
-    logfile_path = os.path.join(cargosail_folder, 'logfile2024.txt')
+    logfile_path =  get_logfile_path()
     
     with open(logfile_path, 'a') as logfile:
         logfile.write(message)
