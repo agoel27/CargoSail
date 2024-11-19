625f523590cf8955e8124b81850dad9eda589eef
Tonio-Codes <anthonymontano146@gmail.com>
2024-11-19 15:58:11 -0800

    got started on the a_star file. Created a priority queue class, and began working on the node and problem class

---
+++ a_star.py
@@ -0,0 +1,70 @@
+class load_unload_problem:
+    def __init__(self,initial_state):
+        self.initial_state = initial_state
+    
+    def goal_test(self):
+        print("hello")
+    
+    def operators(self,node):
+        print("hello")
+
+class priorityQueue:
+    def __init__(self):
+        self.queue = []
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    def push(self,node):
+        self.queue.append(node) 
+    
+    def pop(self):
+        min_cost = self.queue[0].cost()
+        size = len(self.queue)
+        idx = 0
+        for i in range(size):
+            if self.queue[i].cost() < min_cost:
+                min_cost = self.queue[i].cost()
+                idx = i
+        node = self.queue[idx]
+        self.queue.pop(idx)
+        return node
+    
+
+class Node:
+    def __init__(self, current_state):
+        self.state = current_state
+        self.parent = None
+        self.children = []
+        self.cost_g = 0
+        self.cost_h = 0
+
+    
+    def getState(self):
+        return self.state
+    
+    def get_cost(self):
+        return self.cost_g + self.cost_h
+
+    def set_cost_g(self,cost_g):
+        self.cost_g = cost_g
+    
+    def set_cost_h(self,cost_h):
+        self.cost_h = cost_h
+
+    
+
+
+
+
+
+
+
+def a_star(problem,queueing_func):
+    #nodes = makeQueue(Node(problem.initial_state))
+    #loop do
+    #if empty(nodes) then return fail
+        #node = removeFront(nodes)
+    #if problem.goal_test(node.state) succeeds return node
+        #nodes = queueing_function(nodes,expand(node,problem.operators))
+    #end
+    print("hello")
