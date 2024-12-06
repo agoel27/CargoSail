cc563e3264d4d8f7c763a6fc2fabfeff986fdcfa
agoel27 <aryangoel27@gmail.com>
2024-12-06 14:28:08 -0800

    added uniform cost search to balance problem

--- balance_problem.py
+++ balance_problem.py
@@ -8,15 +8,25 @@ class Node:
         self.parent = None
         self.uniform_cost = 0
         self.heuristic_cost = 0
+        self.crane_location = (-1, 0)
 
+    def __lt__(self, other):
+        return self.uniform_cost + self.heuristic_cost < other.uniform_cost + other.heuristic_cost
+    
     def get_state(self):
         return self.data
+    
+    def get_crane_location(self):
+        return self.crane_location
     
     def get_uniform_cost(self):
         return self.uniform_cost
 
     def get_heuristic_cost(self):
         return self.heuristic_cost
+    
+    def set_crane_location(self, coordinates):
+        self.crane_location = coordinates
 
     def set_uniform_cost(self, uniform_cost):
         self.uniform_cost = uniform_cost
@@ -35,85 +45,19 @@ class BalanceProblem:
         """
         Initialize the balance problem with the initial state
         """
-        self.initial_node = Node(initial_state)
+        self.initial_state = initial_state
 
     def get_initial_state(self):
         """
         Return the initial state which represents the ship's incoming manifest
         """
-        return self.initial_node.data
+        return self.initial_state
 
-
-    def expand_node(self, current_node):
-        """
-        Expand the current state by generating all possible new states where
-        every top container is moved to the top of every other vaild column
-        """
-        current_state = current_node.data
-        expanded_nodes = []                            # list to hold all expanded states
-        num_rows = len(current_state)                   # total number of rows
-        num_cols = len(current_state[0])                # total number of columns
-        row_idx_of_top_container = [None] * num_cols    # array to store the row index of the top container in each column
-        row_idx_of_valid_space = [None] * num_cols      # array to store the row index of valid locations for containers in each column
-        
-
-        # find row index of top container for each column and store in 'row_idx_of_top_container'
-        # find row index of valid locations for containers for each column and store in 'row_idx_of_valid_space'
-        """
-        Example:
-        
-          0 1 2 3 4 5 6 7 8  9   10  11
-        0 _ _ _ _ _ _ _ _ H ___ NAN ___
-        1 _ _ _ _ _ _ _ _ I ___ NAN ___
-        2 _ _ _ _ _ _ _ _ J ___ NAN ___         The top containers are C, E, F, H, G
-        3 _ _ _ _ _ _ _ _ K ___ NAN ___     =>  row_idx_of_top_container = [None, None, 5, None, None, 6, 7, None, 0, None, None, 4]
-        4 _ _ _ _ _ _ _ _ L ___ NAN  G          row_idx_of_valid_space   = [7, 7, 4, 7, 7, 5, 6, 7, None, 4, None, 3]
-        5 _ _ C _ _ _ _ _ M NAN NAN NAN
-        6 _ _ B _ _ E _ _ N NAN NAN NAN
-        7 _ _ A _ _ D F _ O NAN NAN NAN
-        """
-        for col in range(num_cols):
-            for row in reversed(range(num_rows)):
-                if current_state[row][col][1] == "UNUSED":
-                    if row != num_rows - 1 and current_state[row+1][col][1] != "NAN":
-                        row_idx_of_top_container[col] = row + 1
-                    row_idx_of_valid_space[col] = row
-                    break
-                elif current_state[row][col][1] != "NAN":
-                    if row == 0:
-                        row_idx_of_top_container[col] = row
-                    continue
-
-        # iterate over each column to find a top container to move
-        for myCol in range(num_cols):
-            myRow = row_idx_of_top_container[myCol]
-            # skip columns with no containers
-            if myRow == None:
-                continue
-            
-            # move container to the top of other valid columns
-            for otherCol in range(num_cols):
-                otherRow = row_idx_of_valid_space[otherCol]
-                if otherRow != None and myCol != otherCol:
-                    expanded_nodes.append(Node(self.move_container(current_state, myRow, myCol, otherRow, otherCol)))
-        
-        return expanded_nodes
-
-    def move_container(self, current_state, myRow, myCol, otherRow, otherCol):
-        """
-        Move the container from (myRow, myCol) to (otherRow, otherCol)
-        """
-        copy_state = copy.deepcopy(current_state)
-        copy_state[otherRow][otherCol] = copy_state[myRow][myCol]
-        copy_state[myRow][myCol] = (0,"UNUSED")
-        return copy_state
-
-    def goal_test(self, current_node):
+    def goal_test(self, current_state):
         """
         Check if the current state satisfies the balance condition
         The goal is achieved if the weights on the left and right sides are less than 10% different
         """
-        current_state = current_node.data
         left_side_weight = 0
         right_side_weight = 0
         
@@ -123,8 +67,115 @@ class BalanceProblem:
                     left_side_weight += row[i][0]
                 else:
                     right_side_weight += row[i][0]
+        print("\n\n", left_side_weight, right_side_weight)
+
+        if left_side_weight == 0 and right_side_weight == 0:
+            return True
+        if left_side_weight == 0 or right_side_weight == 0:
+            return False
         return max(left_side_weight, right_side_weight) / min(left_side_weight, right_side_weight) < 1.1
+
+def calculate_manhattan_dist(my_row, my_col, other_row, other_col):
+    delta_y = abs(my_row - other_row)
+    delta_x = abs(my_col - other_col)
+    return delta_y + delta_x
+
+def move_container(current_state, my_row, my_col, other_row, other_col):
+    """
+    Move the container from (my_row, my_col) to (other_row, other_col)
+    """
+    copy_state = copy.deepcopy(current_state)
+    copy_state[other_row][other_col] = copy_state[my_row][my_col]
+    copy_state[my_row][my_col] = (0, "UNUSED")
+    return copy_state
+
+def expand_node(current_node, explored_states):
+    """
+    Expand the current state by generating all possible new states where
+    every top container is moved to the top of every other vaild column
+    """
+    current_state = current_node.data
+    expanded_nodes = []                             # list to hold all expanded states
+    num_rows = len(current_state)                   # total number of rows
+    num_cols = len(current_state[0])                # total number of columns
+    row_idx_of_top_container = [None] * num_cols    # array to store the row index of the top container in each column
+    row_idx_of_valid_space = [None] * num_cols      # array to store the row index of valid locations for containers in each column
+    crane_coordinates = current_node.get_crane_location()
+
+    # find row index of top container for each column and store in 'row_idx_of_top_container'
+    # find row index of valid locations for containers for each column and store in 'row_idx_of_valid_space'
+    """
+    Example:
         
+      0 1 2 3 4 5 6 7 8  9   10  11
+    0 _ _ _ _ _ _ _ _ H ___ NAN ___
+    1 _ _ _ _ _ _ _ _ I ___ NAN ___
+    2 _ _ _ _ _ _ _ _ J ___ NAN ___         The top containers are C, E, F, H, G
+    3 _ _ _ _ _ _ _ _ K ___ NAN ___     =>  row_idx_of_top_container = [None, None, 5, None, None, 6, 7, None, 0, None, None, 4]
+    4 _ _ _ _ _ _ _ _ L ___ NAN  G          row_idx_of_valid_space   = [7, 7, 4, 7, 7, 5, 6, 7, None, 4, None, 3]
+    5 _ _ C _ _ _ _ _ M NAN NAN NAN
+    6 _ _ B _ _ E _ _ N NAN NAN NAN
+    7 _ _ A _ _ D F _ O NAN NAN NAN
+    """
+    for col in range(num_cols):
+        for row in reversed(range(num_rows)):
+            if current_state[row][col][1] == "UNUSED":
+                if row != num_rows - 1 and current_state[row+1][col][1] != "NAN":
+                    row_idx_of_top_container[col] = row + 1
+                row_idx_of_valid_space[col] = row
+                break
+            elif current_state[row][col][1] != "NAN":
+                if row == 0:
+                    row_idx_of_top_container[col] = row
+                continue
+
+    # iterate over each column to find a top container to move
+    for my_col in range(num_cols):
+        my_row = row_idx_of_top_container[my_col]
+        # skip columns with no containers
+        if my_row == None:
+            continue
+        # move container to the top of other valid columns
+        for other_col in range(num_cols):
+            other_row = row_idx_of_valid_space[other_col]
+            if other_row != None and my_col != other_col:
+                new_state = move_container(current_state, my_row, my_col, other_row, other_col)
+                if tuple(map(tuple, new_state)) not in explored_states:
+                    node_to_add = Node(new_state)
+                    node_to_add.set_uniform_cost(current_node.uniform_cost + calculate_manhattan_dist(crane_coordinates[0], crane_coordinates[1], my_row, my_col) + calculate_manhattan_dist(my_row, my_col, other_row, other_col))
+                    node_to_add.set_crane_location((other_row, other_col))
+                    expanded_nodes.append(node_to_add)
+    
+    current_node.add_children(expanded_nodes)
+    return expanded_nodes
+
+def a_star(manifest_data):
+    problem = BalanceProblem(manifest_data)
+    initial_state = problem.get_initial_state()
+
+    queue = []
+    heappush(queue, Node(initial_state))
+
+    explored_states = set()
+    explored_states.add(tuple(map(tuple, initial_state)))
+
+    while(queue):
+        current_node = heappop(queue)
+        if problem.goal_test(current_node.get_state()):
+            return current_node
+        print("Expanding Node with cost ", current_node.get_uniform_cost(), ":")
+        for row in current_node.get_state():
+            print(row)
+        child_nodes = expand_node(current_node, explored_states)
+        for child_node in child_nodes:
+            heappush(queue, child_node)
+            explored_states.add(tuple(map(tuple, child_node.get_state())))
+        print("\n\nNodes in frontier:\n")
+        for node in queue:
+            print("\nCost: ", node.get_uniform_cost())
+            for row in node.get_state():
+                print(row)
+    return 0
 
 example_ship_data = [
     [(109, "UNUSED"), (0, "UNUSED"), (0, "UNUSED"), (0, "UNUSED"),   (0, "UNUSED"),   (0, "UNUSED"),   (100, "UNUSED"), (0, "cat"),      (0, "UNUSED"), (0, "UNUSED"), (0, "UNUSED"),  (1, "NAN")],
@@ -137,13 +188,14 @@ example_ship_data = [
     [(0, "bear"),     (0, "bear"),   (0, "bear"),   (0, "bear"),     (0, "bear"),     (0, "bear"),     (0, "bear"),     (0, "bear"),     (0, "NAN"),    (0, "UNUSED"), (0, "NAN"),     (0, "NAN")]
 ]
 
-# states = BalanceProblem(example_ship_data).expand_node(Node(example_ship_data))
+example_manifest_data = [
+    [(0, "UNUSED"), (0, "UNUSED"), (50, "DOG"), (0, "UNUSED")],
+    [(0, "UNUSED"), (0, "UNUSED"), (50, "DOG"), (0, "NAN")],
+    [(0, "UNUSED"), (1, "CAT"), (0, "NAN"), (0, "NAN")],
+    [(0, "UNUSED"), (110, "CAT"), (0, "NAN"), (0, "NAN")]
+]
 
-# for state in states:
-#     for row in state.data:
-#         print(row)
+solution_node = a_star(example_manifest_data)
 
-#     print("end")
-
-def a_star():
-    return 0
+for row in solution_node.data:
+    print(row)
